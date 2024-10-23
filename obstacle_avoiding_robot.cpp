77de30582f39b563b78c937141709e64d80d6da9
Amcool <Alphonceomondi273@gmail.com>
2024-10-23 10:56:52 +0300

    Set the pins for relay , trigger and echo.

--- obstacle_avoiding_robot.cpp
+++ obstacle_avoiding_robot.cpp
@@ -1,5 +1,15 @@
 const int trigPin = 8;
 const int echoPin = 9;
 
-const int relay = 7;
+const int relayPin = 7;
 const int thresholdDistance = 20; // 20cm
+
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(relayPin, OUTPUT);
+    
+}
